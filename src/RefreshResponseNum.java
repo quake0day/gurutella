94ce2c8975b29fcd8562e6f49941ed576c2bed99
quake0day <quake0day@gmail.com>
2012-12-03 01:29:46 -0500

    刷新界面

--- src/RefreshResponseNum.java
+++ src/RefreshResponseNum.java
@@ -26,9 +26,16 @@ public class RefreshResponseNum  implements Runnable{
 		// TODO Auto-generated method stub
 		while(true){
 			response = qrl.length();
-			System.out.println(response+" response received.");
-            try {
+			System.out.print(response+" response received.");
+			try {
 				Thread.sleep(10);
+			} catch (InterruptedException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			System.out.print("\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b");
+            try {
+				Thread.sleep(100);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				//e.printStackTrace();
